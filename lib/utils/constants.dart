184fbfee883965c9cc6143ab3456955a64efc097
Ahmed Ibrahim Elsabaa <72297175+ahmed101ibrahiem@users.noreply.github.com>
2024-05-18 14:10:07 +0300

    first commit

---
+++ lib/utils/constants.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:flutter/material.dart';
+
+class AppConstant{
+  // text
+  static const String appName = 'Expense Tacker';
+
+
+  // color
+  static const Color blue = Color(0XFF00B2E7);
+  static const Color orange = Color(0XFFFF8D6C);
+  static const Color purple = Color(0XFFE064F7);
+}
